382c02e673df651362f5c412a5dd33b28285c27c
hubo <hubo@1057277483@qq.com>
2018-12-27 15:27:06 +0800

    modefy process feat and ali tool

--- tools/process-feat-ali.sh
+++ tools/process-feat-ali.sh
@@ -14,13 +14,17 @@ split=$3
 outdir=$4
 mkdir -p $outdir
 awk '{printf("%d\n",NF);}' $ali > $outdir/ali.len
-paste $outdir/ali.len $ali|sort -nk 1 |while read len aliinfo
-do
-	echo $aliinfo 
-done > $outdir/sort_$alifile || exit 1
+paste $outdir/ali.len $ali|sort -nk 1 |\
+   	awk '{for(i=2;i<=NF;++i)printf("%s ",$i); printf("\n")}' > $outdir/sort_$alifile || exit 1
+#paste $outdir/ali.len $ali|sort -nk 1 |while read len aliinfo
+#do
+#	echo $aliinfo 
+#done > $outdir/sort_$alifile || exit 1
 
 python map-feat-ali.py $feat_scp $outdir/sort_$alifile $outdir/sort_$feat_scpfile || exit 1
 
+python map-feat-ali.py $outdir/sort_$alifile $outdir/sort_$feat_scpfile $outdir/mapsort_$alifile
+
 python split_feat_ali.py $outdir/sort_$feat_scpfile  \
-	$outdir/sort_$alifile $split \
+	$outdir/mapsort_$alifile $split \
 	$outdir/split || exit 1
