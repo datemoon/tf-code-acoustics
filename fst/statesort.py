a6b737f2f878062beafd1d488fd9686e0f56a7a9
hubo <hubo@1057277483@qq.com>
2019-02-18 19:07:51 +0800

    add statesort

---
+++ fst/statesort.py
@@ -0,0 +1,33 @@
+
+import logging
+from fst import *
+
+def StateSort(fst, order):
+    if len(order) != fst.NumStates():
+        logging.info("StateSort: Bad order vector size: %d" % len(order))
+        return
+
+    if fst.Start() == kNoStateId:
+        return
+    done = [ False for x in range(len(order))]
+    arcsa = []
+    arcsb = []
+    start = fst.Start()
+    fst.SetStart(order[start])
+    for s1 in range(fst.NumStates()):
+        if done[s1]:
+            continue
+        state1 = fst.GetState(s1)
+        while done[s1] is False:
+            s2 = order[s1]
+            if done[s2] is False:
+                state2 = fst.GetState(s2)
+            
+            # change nextstate
+            for arc in state1.GetArcs():
+                arc._nextstate = order[arc._nextstate]
+            fst.SetState(s2, state1)
+            done[s1] = True
+            
+            s1 = s2
+            state1 = state2
