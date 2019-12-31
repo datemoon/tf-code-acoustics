02a64dc80353444c51ed828d625fa5b14fe9ed8e
hubo <hubo@1057277483@qq.com>
2019-12-31 20:10:18 +0800

    add distribute code

--- tf2.0-model/distributed_test.py
+++ tf2.0-model/distributed_test.py
@@ -26,71 +26,79 @@ from nnet_conf import nnet_conf
 from tf_chain_py_api import chainloss,chainxentloss
 from fst import Fst2SparseMatrix
 
-@tf.function
-def chainloss_fn(outputs, deriv_weights,
-        indexs, in_labels, weights, statesinfo, num_states,
-        label_dim,
-        den_indexs, den_in_labels, den_weights, den_statesinfo, den_num_states,
-        den_start_state, delete_laststatesuperfinal,
-        l2_regularize, leaky_hmm_coefficient, xent_regularize,
-        time_major = True):
-    chain_loss = chainloss(outputs, deriv_weights,
-            indexs, in_labels, weights, statesinfo, num_states,
-            label_dim,
-            den_indexs, den_in_labels, den_weights, den_statesinfo, den_num_states,
-            den_start_state, delete_laststatesuperfinal,
-            l2_regularize, leaky_hmm_coefficient, xent_regularize,
-            time_major = True)
-    return chain_loss
+class Train():
+    def __init__(self, strategy):
+        self.model = CommonModel(nnet_conf)
+        self.strategy = strategy
+        # Instantiate an optimizer.
+        self.optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+        den_fst = '../chain_source/den.fst'
+        den_indexs, den_in_labels, den_weights, den_statesinfo, den_num_states, den_start_state, laststatesuperfinal = Fst2SparseMatrix(den_fst)
+        self.leaky_hmm_coefficient = 0.1
+        self.l2_regularize = 0.00005
+        self.xent_regularize = 0.0
+        self.delete_laststatesuperfinal = True
+        self.label_dim = 3766
+        self.den_indexs = np.reshape(den_indexs,[-1]).tolist()
+        self.den_in_labels = np.reshape(den_in_labels , [-1]).tolist()
+        self.den_weights = np.reshape(den_weights, [-1]).tolist()
+        self.den_statesinfo = np.reshape(den_statesinfo, [-1]).tolist()
+        self.den_num_states = den_num_states
+        self.den_start_state = den_start_state
+        self.laststatesuperfinal = laststatesuperfinal
 
-
-def TrainStep(model, inputs):
-    #feat_mat, label, length, lat_list = inputs
-    start1 = time.time()
-    feat_mat, label, length, indexs, in_labels, weights, statesinfo, num_states = inputs
-    print("feat_mat:",feat_mat.shape)
-    end1 = time.time()
-    logging.info("time:"+str(end1-start1))
+    def TrainStep(self, inputs):
+        #feat_mat, label, length, lat_list = inputs
+        start1 = time.time()
+        feat_mat, label, length, indexs, in_labels, weights, statesinfo, num_states = inputs
+        print("feat_mat:",feat_mat.shape)
+        end1 = time.time()
+        logging.info("time:"+str(end1-start1))
+        
+        deriv_weights = label
+        #indexs, in_labels, weights, statesinfo, num_states = lat_list
+        with tf.GradientTape() as tape:
+            start2 = time.time()
+            print("-----------model start------------------")
+            outputs = self.model(feat_mat)
+            print("----------end outputs:",outputs.shape)
+            end2 = time.time()
+            start3 = time.time()
+            chain_loss = chainloss(outputs, deriv_weights,
+                    indexs, in_labels, weights, statesinfo, num_states,
+                    self.label_dim,
+                    self.den_indexs, self.den_in_labels, self.den_weights, self.den_statesinfo, self.den_num_states,
+                    self.den_start_state, self.delete_laststatesuperfinal,
+                    self.l2_regularize, self.leaky_hmm_coefficient, self.xent_regularize,
+                    time_major = True)
+            end3 = time.time()
+            print("chain_loss time:",end3-start3)
+            chain_mean_loss = chain_loss[0]
+            #model.summary()
+            # Compute the loss value for this minibatch.
+            #loss_value = loss_fn(label, outputs)
     
-    deriv_weights = label
-    #indexs, in_labels, weights, statesinfo, num_states = lat_list
-    with tf.GradientTape() as tape:
-        start2 = time.time()
-        print("-----------model start------------------")
-        outputs = model(feat_mat)
-        print("----------end outputs:",outputs.shape)
-        end2 = time.time()
-        start3 = time.time()
-        chain_loss = chainloss(outputs, deriv_weights,
-                indexs, in_labels, weights, statesinfo, num_states,
-                label_dim,
-                den_indexs, den_in_labels, den_weights, den_statesinfo, den_num_states,
-                den_start_state, delete_laststatesuperfinal,
-                l2_regularize, leaky_hmm_coefficient, xent_regularize,
-                time_major = True)
-        end3 = time.time()
-        print("chain_loss time:",end3-start3)
-        chain_mean_loss = chain_loss[0]
-        #model.summary()
-        # Compute the loss value for this minibatch.
-        #loss_value = loss_fn(label, outputs)
-
-    # Use the gradient tape to automatically retrieve
-    # the gradients of the trainable variables with respect to the loss.
-    start4 = time.time()
-    grads = tape.gradient(chain_mean_loss, model.trainable_weights)
-    end4 = time.time()
-
-    grads, gradient_norms = tf.clip_by_global_norm(grads, 5.0, use_norm=None)
+        # Use the gradient tape to automatically retrieve
+        # the gradients of the trainable variables with respect to the loss.
+        start4 = time.time()
+        grads = tape.gradient(chain_mean_loss, self.model.trainable_weights)
+        end4 = time.time()
     
-    # Run one step of gradient descent by updating
-    # the value of the variables to minimize the loss.
-    start5 = time.time()
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-    end5 = time.time()
-    
-    print("nnet time:%f, loss time:%f, grad time:%f, apply_gradients time:%f" % (end2-start2,end3-start3,end4-start4,end5-start5))
-    return chain_mean_loss
+        grads, gradient_norms = tf.clip_by_global_norm(grads, 5.0, use_norm=None)
+        
+        # Run one step of gradient descent by updating
+        # the value of the variables to minimize the loss.
+        start5 = time.time()
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_weights))
+        end5 = time.time()
+        
+        print("nnet time:%f, loss time:%f, grad time:%f, apply_gradients time:%f" % (end2-start2,end3-start3,end4-start4,end5-start5))
+        return chain_mean_loss
+
+    def RunStep(self, inputs):
+        per_replica_loss = self.strategy.experimental_run_v2(
+                train.TrainStep, args=(inputs,))
+        return per_replica_loss
 
 if __name__ == '__main__':
     #path = '/search/speech/hubo/git/tf-code-acoustics/chain_source_7300/8-cegs-scp/'
@@ -116,21 +124,8 @@ if __name__ == '__main__':
     io_read.Reset(shuffle = False)
     batch_num = 0
     
-    # Instantiate an optimizer.
-    optimizer = tf.keras.optimizers.SGD(learning_rate=1e-1)
     # Instantiate a loss function.
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
-    den_fst = '../chain_source/den.fst'
-    den_indexs, den_in_labels, den_weights, den_statesinfo, den_num_states, den_start_state, laststatesuperfinal = Fst2SparseMatrix(den_fst)
-    leaky_hmm_coefficient = 0.1
-    l2_regularize = 0.00005
-    xent_regularize = 0.0
-    delete_laststatesuperfinal = True
-    label_dim = 3766
-    den_indexs = np.reshape(den_indexs,[-1]).tolist()
-    den_in_labels = np.reshape(den_in_labels , [-1]).tolist()
-    den_weights = np.reshape(den_weights, [-1]).tolist()
-    den_statesinfo = np.reshape(den_statesinfo, [-1]).tolist()
+    #loss_fn = tf.keras.losses.SparseCategoricalCrossentropy()
 
     loss_value = 0.0
     
@@ -161,37 +156,52 @@ if __name__ == '__main__':
     num_gpu=1
     devices = ['/device:GPU:{}'.format(i) for i in range(num_gpu)]
     strategy = tf.distribute.MirroredStrategy(devices)
-    input_context = tf.distribute.InputContext(num_input_pipelines=1,
+    input_context = tf.distribute.InputContext(num_input_pipelines=num_gpu,
             input_pipeline_id=0,
-            num_replicas_in_sync=1)
+            num_replicas_in_sync=num_gpu)
 
     with strategy.scope():
+        train = Train(strategy)
         def dataset_fn(input_context):
             dataset = tf.data.Dataset.from_generator(Gen, output_types=(tf.float32,tf.float32,tf.int32,tf.int32,tf.int32,tf.float32,tf.int32,tf.int32),output_shapes=None,args=None)
             return dataset.shard(
                     input_context.num_input_pipelines, input_context.input_pipeline_id)
+        
+        def distributed_train(model, one_batch):
+            per_replica_loss = strategy.experimental_run_v2(
+                    train.TrainStep, args=(one_batch))
+            return per_replica_loss
+        
+        distributed_train = tf.function(distributed_train)
 
-        model = CommonModel(nnet_conf)
+
 
         train_dist_dataset = strategy.experimental_distribute_datasets_from_function(dataset_fn)
 
         print('Training...')
         total_loss = 0.0
         num_train_batches = 0.0
+        batch_loss = 0.0
 
         for one_batch  in train_dist_dataset:
             print('*******************start************************')
-            #print('****one batch*******',one_batch[2],num_train_batches)
+            print('****one batch*******',one_batch[2],num_train_batches)
             #time.sleep(1)
-            per_replica_loss = strategy.experimental_run_v2(
-                    TrainStep, args=(model, one_batch))
+            #per_replica_loss = strategy.experimental_run_v2(
+            #        train.TrainStep, args=(one_batch))
+            per_replica_loss = train.RunStep(one_batch)
+            #per_replica_loss = distributed_train(model, one_batch)
             print('num_train_batches: %f per_replica_loss: %f'%(num_train_batches,per_replica_loss))
+            batch_loss += per_replica_loss
             total_loss += strategy.reduce(
-                    tf.distribute.ReduceOp.SUM, per_replica_loss, axis=None)
+                    tf.distribute.ReduceOp.MEAN, per_replica_loss, axis=None)
+                    #tf.distribute.ReduceOp.SUM, per_replica_loss, axis=None)
 
             num_train_batches += 1
             if num_train_batches % 20 == 0:
-                print("trian loss:%f" % (total_loss/num_train_batches))
+                print("num_train_batches:%d,train loss:%f, batch loss:%f" % (num_train_batches, total_loss/num_train_batches,batch_loss/20))
+                batch_loss = 0.0
+                train.model.summary()
                 #time.sleep(5)
 
 
@@ -200,10 +210,10 @@ if __name__ == '__main__':
         #weights = model.get_weights()
         #new_model.set_weights(weights)
         #model.save_weights('path_to_my_weights.h5')
-        model.summary()
+        train.model.summary()
         #model.SaveModelWeights('model/path_to_my_weights.h5-1')
         #model.SaveModelWeights('model/path_to_my_weights.h5', save_format='h5')
-        del model
+        del train.model
         #new_model = CommonModel.ReStoreModel(nnet_conf, 'model/path_to_my_weights.h5-1')
         #new_model.summary()
         #new_model.SaveModelWeights('model/path_to_my_weights.h5-2')
